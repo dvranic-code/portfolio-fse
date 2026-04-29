7e89ccf6497692eb9449d2831eb8ff66dec36ca0
Dejan Rudic Vranic <dejanrudicvranic@gmail.com>
2026-04-29 17:47:15 +0200

    cta banner done

---
+++ patterns/cta-banner.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Title: CTA Banner
+ * Slug: portfolio-fse/cta-banner
+ * Categories: portfolio-fse
+ * Description: A call to action banner.
+ * Keywords: cta, banner, portfolio
+ * 
+ */
+?>
+<!-- wp:group {"style":{"elements":{"link":{"color":{"text":"var:preset|color|foreground-light"}}},"spacing":{"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50","top":"var:preset|spacing|80","bottom":"var:preset|spacing|80"}}},"backgroundColor":"background","textColor":"foreground-light","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-foreground-light-color has-background-background-color has-text-color has-background has-link-color" style="padding-top:var(--wp--preset--spacing--80);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--80);padding-left:var(--wp--preset--spacing--50)"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"textAlign":"center","style":{"spacing":{"margin":{"bottom":"0"}}},"fontSize":"xx-large"} -->
+<h2 class="wp-block-heading has-text-align-center has-xx-large-font-size" style="margin-bottom:0">Let's <mark style="background-color:rgba(0, 0, 0, 0)" class="has-inline-color has-accent-secondary-color">collaborate</mark>.</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"elements":{"link":{"color":{"text":"var:preset|color|surface-light"}}},"spacing":{"margin":{"top":"0"}}},"textColor":"surface-light"} -->
+<p class="has-text-align-center has-surface-light-color has-text-color has-link-color" style="margin-top:0">Open source, workshops, speaking. Not client work. If that lines up — say hi.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"style":{"spacing":{"margin":{"top":"var:preset|spacing|70"}}},"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons" style="margin-top:var(--wp--preset--spacing--70)"><!-- wp:button {"textAlign":"center","className":"is-style-primary"} -->
+<div class="wp-block-button is-style-primary"><a class="wp-block-button__link has-text-align-center wp-element-button">hello@wapuu.dev</a></div>
+<!-- /wp:button -->
+
+<!-- wp:button {"className":"is-style-secondary"} -->
+<div class="wp-block-button is-style-secondary"><a class="wp-block-button__link wp-element-button">schedule a call</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
